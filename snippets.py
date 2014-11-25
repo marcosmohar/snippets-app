fcadd82283c2ee8d22030bb2eee4205dc2916169
Marcos Mohar <elcabeca@gmail.com>
2014-11-24 22:56:29 -0600

    Added  parser constructor and main functions

--- snippets.py
+++ snippets.py
@@ -1,5 +1,7 @@
-import logging
+import argparse
 import csv
+import logging
+import sys
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="output.log", level=logging.DEBUG)
@@ -8,6 +10,7 @@ def put(name, snippet, filename):
 	""" Store snippet with an associated name in the csv file"""
 	logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
 	logging.debug("Oppening file")
+
 	with open(filename, "a") as f:
 		writer = csv.writer(f)
 		logging.debug("Writing snippet to file")
@@ -15,4 +18,21 @@ def put(name, snippet, filename):
 	logging.debug("Write succesful")
 	return name, snippet
 
+def make_parser():
+	""" Construct the command line parser """
+	logging.info("Constructing parser")
+	description = "Store and retrieve snippets of text"
+	parser = argparse.ArgumentParser(description=description)
+
+	return parser
+
+
+def main():
+	""" Main function """
+	logging.info("Starting snippets")
+	parser = make_parser()
+	arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+	main()
 
